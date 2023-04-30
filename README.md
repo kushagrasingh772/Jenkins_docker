7d801517a48481bfc29a4f02dc98cd61689e406a
Kushagra Singh <52888710+kushagrasingh772@users.noreply.github.com>
2023-04-30 16:57:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Jenkins_docker
-Jenkins docker project
+Demonstration of Docker to implement a simple 3 tier architecture
+
+* frontend will be able to access the mid-tier
+* mid-tier will be able to access the db
+
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const port = 3001;
+const routes = require("./routes");
+const mongo_path = process.env.MONGO_URI
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(`${mongo_path}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use("/api", routes);
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+  });
+}
